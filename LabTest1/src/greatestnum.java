8b93db2beb9f09757117a960b73841909f82a41b
Bhushan M Patil <Bhushan M Patil@LAPTOP-1QO6N7R1>
2022-09-08 12:21:36 +0530

    commit

---
+++ LabTest1/src/greatestnum.java
@@ -0,0 +1,20 @@
+
+public class greatestnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1=30;
+		int n2=22;
+		int n3=17;
+		 if( n1 >= n2 && n1>= n3)
+	            System.out.println(n1 + " is the largest number.");
+
+	        else if (n2 >= n1 && n2>= n3)
+	            System.out.println(n2 + " is the largest number.");
+
+	        else
+	            System.out.println(n3+ " is the largest number.");
+
+	}
+
+}
---
+++ LabTest1/src/switchcase.java
@@ -0,0 +1,23 @@
+
+public class switchcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=12;
+		int b=4;
+		String cha="*";
+		switch(cha)
+		{
+		case "+":System.out.println(a+b);
+		break;
+		case "-":System.out.println(a-b);
+		break;
+		case "*":System.out.println(a*b);
+		break;
+		case "/":System.out.println(a/b);
+		break;
+		default: System.out.println("wrong input" );
+		}
+	}
+
+}
